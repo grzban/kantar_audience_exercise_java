2a67cdec3309bb07103c95e42ba5a9d21eba03b7
GB <grzegorz.banaszek@gmail.com>
2021-04-25 17:51:12 +0200

    Added cleaning after test

--- src/test/java/com/kantar/sessionsjob/facade/FacadeTest.java
+++ src/test/java/com/kantar/sessionsjob/facade/FacadeTest.java
@@ -2,22 +2,23 @@ package com.kantar.sessionsjob.facade;
 
 import com.kantar.sessionsjob.util.ResourcePathUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Slf4j
 class FacadeTest {
+    private String actualSessionBeansFile;
 
     @Test
     void prepareSessionReport() throws IOException {
         String inputFile = ResourcePathUtil.getStringAbsolutePathToResource(this, "input-statements.psv");
         String expectedSessionsFile = ResourcePathUtil.getStringAbsolutePathToResource(this, "expected-sessions.psv");
-        String actualSessionBeansFile = ResourcePathUtil.getStringAbsolutePathToResource(this, "") + "/actual-sessionBeans.psv";
+        actualSessionBeansFile = ResourcePathUtil.getStringAbsolutePathToResource(this, "") + "/actual-sessionBeans.psv";
+        log.info("actualSessionBeansFile: {}", actualSessionBeansFile);
 
         new Facade(inputFile, actualSessionBeansFile).prepareSessionReport();
 
@@ -25,5 +26,14 @@ class FacadeTest {
         byte[] expected = Files.readAllBytes(ResourcePathUtil.getPathFromString(expectedSessionsFile));
 
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @AfterEach
+    void tearDown() {
+        try {
+            Files.delete(ResourcePathUtil.getPathFromString(actualSessionBeansFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
